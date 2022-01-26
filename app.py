80437caec03deee46558dc336388c62996737c3e
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2022-01-25 19:12:46 -0500

    .

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def delete(todo_id):
 if __name__ == "__main__":
     db.create_all()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(port=port, debug=True)
